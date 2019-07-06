2c53cfb73533bfe9d7407c03665721c2a8dfee56
ThomasSwindells <ThomasSwindells@users.noreply.github.com>
2019-07-06 20:06:02 +0000

    Fix up whitespace, Add makefile Create key, classify traffic direction and ignore zero payload packets.
    Traffic direction classification is basic using port numbers as indicator (assumption is connections will be to port 80 or 443 or some other system port. Other ways this could be done include tracking the connection from the beginning and so observing the direction, knowing which IPs are internall, or looking at handshakes or other indicators.

--- StreamIdentify.cpp
+++ StreamIdentify.cpp
@@ -21,68 +21,85 @@ using namespace std;
 
 void packetHandler(u_char *userData, const struct pcap_pkthdr* pkthdr, const u_char* packet);
 
-int main() {
-  pcap_t *descr;
-  char errbuf[PCAP_ERRBUF_SIZE];
+int main(int argc, const char* argv[]) {
+	pcap_t *descr;
+	char errbuf[PCAP_ERRBUF_SIZE];
 
-  // open capture file for offline processing
-  descr = pcap_open_offline("http.cap", errbuf);
-  if (descr == NULL) {
-      cout << "pcap_open_live() failed: " << errbuf << endl;
-      return 1;
-  }
+	if(argc < 2) {
+		cout << "Missing filename" << endl;
+	  return 2;
+	}
 
-  // start packet processing loop, just like live capture
-  if (pcap_loop(descr, 0, packetHandler, NULL) < 0) {
-      cout << "pcap_loop() failed: " << pcap_geterr(descr);
-      return 1;
-  }
+	// open capture file for offline processing
+	descr = pcap_open_offline(argv[1], errbuf);
+	if (descr == NULL) {
+			cout << "pcap_open_live() failed: " << errbuf << endl;
+			return 1;
+	}
 
-  cout << "capture finished" << endl;
+	// start packet processing loop, just like live capture
+	if (pcap_loop(descr, 0, packetHandler, NULL) < 0) {
+			cout << "pcap_loop() failed: " << pcap_geterr(descr);
+			return 1;
+	}
 
-  return 0;
+	cout << "capture finished" << endl;
+
+	return 0;
+}
+
+string makeKey(char *sourceIp, u_int sourcePort, char *destIp, u_int destPort) {
+	string retVal;
+	//Typically initiator will be communicating with ephemeral whist the target will be in the system (or at least user) ports
+	//Ordering by port number is a good way to get a stable key for the connection most likely starting with the initiator
+	if(destPort <= sourcePort) {
+		retVal = string(sourceIp) + ":"+to_string(sourcePort)+"-"+string(destIp)+":"+to_string(destPort);
+	} else {
+		retVal = string(destIp)+":"+to_string(destPort) + "-" + string(sourceIp) + ":"+to_string(sourcePort);
+	}
+
+	return retVal;
 }
 
 void packetHandler(u_char *userData, const struct pcap_pkthdr* pkthdr, const u_char* packet) {
-  const struct ether_header* ethernetHeader;
-  const struct ip* ipHeader;
-  const struct tcphdr* tcpHeader;
-  char sourceIp[INET_ADDRSTRLEN];
-  char destIp[INET_ADDRSTRLEN];
-  u_int sourcePort, destPort;
-  u_char *data;
-  int dataLength = 0;
-  string dataStr = "";
+	const struct ether_header* ethernetHeader;
+	const struct ip* ipHeader;
+	const struct tcphdr* tcpHeader;
+	char sourceIp[INET_ADDRSTRLEN];
+	char destIp[INET_ADDRSTRLEN];
+	u_int sourcePort, destPort;
+	u_char *data;
+	int dataLength = 0;
+	string dataStr = "";
+	string key;
+	string flow; 
 
-  ethernetHeader = (struct ether_header*)packet;
-  if (ntohs(ethernetHeader->ether_type) == ETHERTYPE_IP) {
-      ipHeader = (struct ip*)(packet + sizeof(struct ether_header));
-      inet_ntop(AF_INET, &(ipHeader->ip_src), sourceIp, INET_ADDRSTRLEN);
-      inet_ntop(AF_INET, &(ipHeader->ip_dst), destIp, INET_ADDRSTRLEN);
+	ethernetHeader = (struct ether_header*)packet;
+	if (ntohs(ethernetHeader->ether_type) == ETHERTYPE_IP) {
+		ipHeader = (struct ip*)(packet + sizeof(struct ether_header));
+		inet_ntop(AF_INET, &(ipHeader->ip_src), sourceIp, INET_ADDRSTRLEN);
+		inet_ntop(AF_INET, &(ipHeader->ip_dst), destIp, INET_ADDRSTRLEN);
 
-      if (ipHeader->ip_p == IPPROTO_TCP) {
-          tcpHeader = (tcphdr*)(packet + sizeof(struct ether_header) + sizeof(struct ip));
-          sourcePort = ntohs(tcpHeader->source);
-          destPort = ntohs(tcpHeader->dest);
-          data = (u_char*)(packet + sizeof(struct ether_header) + sizeof(struct ip) + sizeof(struct tcphdr));
-          dataLength = pkthdr->len - (sizeof(struct ether_header) + sizeof(struct ip) + sizeof(struct tcphdr));
-
-          // convert non-printable characters, other than carriage return, line feed,
-          // or tab into periods when displayed.
-          for (int i = 0; i < dataLength; i++) {
-              if ((data[i] >= 32 && data[i] <= 126) || data[i] == 10 || data[i] == 11 || data[i] == 13) {
-                  dataStr += (char)data[i];
-              } else {
-                  dataStr += ".";
-              }
-          }
-
-          // print the results
-          cout << sourceIp << ":" << sourcePort << " -> " << destIp << ":" << destPort << endl;
-          if (dataLength > 0) {
-              cout << dataStr << endl;
-          }
-      }
-  }
+		if (ipHeader->ip_p == IPPROTO_TCP) {
+			tcpHeader = (tcphdr*)(packet + sizeof(struct ether_header) + sizeof(struct ip));
+			sourcePort = ntohs(tcpHeader->source);
+			destPort = ntohs(tcpHeader->dest);
+			dataLength = pkthdr->len - (sizeof(struct ether_header) + sizeof(struct ip) + sizeof(struct tcphdr));
+			
+			//we don't care about 0 byte packets - they are acks (or similar) and not true communication
+			if(dataLength > 0) { 
+				if(destPort < sourcePort) {
+					flow = "request";
+				} else {
+					flow = "response";
+				}
+			
+			
+				key = makeKey(sourceIp, sourcePort, destIp, destPort);
+				// print the results
+				cout << key << " " << flow <<  " [" << dataLength << "]" << endl;
+			}
+		}
+	}
 }
 
